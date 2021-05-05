52f86321135da0c983739ecd8658622ac5d9af3b
fxrcha <truetype24@gmail.com>
2021-05-06 00:14:14 +0900

    refactor: rewrite file structure

--- test/fetch_bigsur.py
+++ test/fetch_bigsur.py
@@ -1,9 +1,11 @@
 from datetime import date, datetime
 from pyapple import Client
 
+import pprint
+
 
 async def sadf():
-    print(
+    pprint.pprint(
         await Client.get_macos(
             title="macOS Big Sur",
             version="11.3.1",
