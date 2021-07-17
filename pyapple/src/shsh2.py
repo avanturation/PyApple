30dfd1e1269518a745e5bfcec6a17808847a841a
fxrcha <truetype24@gmail.com>
2021-07-17 16:55:45 +0900

    Using @DanTheMann15 tsschecker for windows, @1Conan tsschecker for linux, and @thimstar tsschecker for macOS

--- pyapple/src/shsh2.py
+++ pyapple/src/shsh2.py
@@ -1,3 +1,9 @@
 class SHSH2:
     def __init__(self) -> None:
         pass
+
+    async def run_tsschecker(self, **kwargs):
+        pass
+
+    async def save_blobs(self, identifier, version):
+        pass
