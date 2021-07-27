18ce642ba50eddffec6bd13c6f9b83bcc00f0efb
fxrcha <truetype24@gmail.com>
2021-07-27 20:51:43 +0900

    Examples for SWSCAN

---
+++ example/swscan/download_monterey.py
@@ -0,0 +1,46 @@
+import asyncio
+import aiohttp
+from tqdm import tqdm
+
+from pyapple import Apple
+
+client = Apple()
+
+
+async def download(url: str, name: str, buildid: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            with tqdm(
+                total=int(resp.headers["content-length"], 0),
+                unit="",
+                desc=f"Downloading {name}: ",
+                unit_divisor=1024,
+                ascii=True,
+                unit_scale=True,
+            ) as bar:
+                with open(f"{buildid}/{name}", "wb") as f:
+                    async for chunk in resp.content.iter_chunked(1024):
+                        f.write(chunk)
+                        bar.update(len(chunk))
+
+
+async def download_monterey():
+    monterey_result = await client.search_macos(
+        title="macOS Monterey", catalog_id="developerbeta"
+    )
+
+    real_monterey = monterey_result[0]
+    print("Latest macOS Moneterey build details:")
+    print(f"Title: {real_monterey.title}")
+    print(f"Version: {real_monterey.version}")
+    print(f"Build ID: {real_monterey.buildid}")
+    print(f"Uploaded date: {real_monterey.postdate}")
+
+    print("Starting download")
+    for package in real_monterey.packages:
+        await download(package.url, package.filename, real_monterey.buildid)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(download_monterey())
---
+++ example/swscan/fetch_macos.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from pyapple import Apple
+
+client = Apple()
+
+SUF = {"1": "publicrelease", "2": "publicbeta", "3": "developerbeta"}
+
+
+async def fetch_all(catalog_id: str):
+    result = await client.fetch_macos(catalog_id)
+
+    print("Available macOS Products:")
+
+    for macos in result:
+        print(f"{macos.title} {macos.version} {macos.buildid}")
+
+
+if __name__ == "__main__":
+    print("1. Public Releases")
+    print("2. Public Beta Releases")
+    print("3. Developer Beta Releases")
+
+    selection = input("Select: ")
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(fetch_all(SUF[selection]))
