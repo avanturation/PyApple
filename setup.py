9ae96f657b23d68e82b47c3c792c6136e254697c
fxrcha <truetype24@gmail.com>
2021-12-09 20:00:02 +0900

    PyApple 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="PyApple",
-    version="2.0.0",
+    version="2.0.1",
     license="MIT",
     author="fxrcha",
     author_email="truetype24@gmail.com",
